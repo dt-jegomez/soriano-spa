5d57077f62f851d77078f2df2e567a60c83541ea
JuanGomez <go.juangomez23@gmail.com>
2020-06-20 18:04:51 -0500

    element configurtacion

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,7 +1,16 @@
 <template>
-  <card :title="$t('home')">
+  <!-- <card :title="$t('home')">
     {{ $t('you_are_logged_in') }}
-  </card>
+  </card> -->
+  <el-card class="box-card">
+  <div slot="header" class="clearfix">
+    <span>Card name</span>
+    <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button>
+  </div>
+  <div v-for="o in 4" :key="o" class="text item">
+    {{'List item ' + o }}
+  </div>
+</el-card>
 </template>
 
 <script>
